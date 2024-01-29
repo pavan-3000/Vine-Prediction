a56426bce96bb6804dd9644e339f1a31985cdc47
pavan-3000 <pavansingh3000@gmail.com>
2024-01-29 18:49:36 +0530

    requirements and setups

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+
+setup(
+    name="vine project",
+    version="0.0.0.0",
+    author="pavan",
+    author_email="pavansingh3000@gmail.com",
+    packages=find_packages()
+)
